788cbef8202c941c149cdac524287d07a636be9a
akmamun <almamunkhan04@gmail.com>
2022-09-13 22:02:00 +0600

    added gorm pagination package with example

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"gorm.io/gorm"
+	"time"
+)
+
+type Example struct {
+	Id        int        `json:"id"`
+	Data      string     `json:"data" binding:"required"`
+	CreatedAt *time.Time `json:"created_at,string,omitempty"`
+	UpdatedAt *time.Time `json:"updated_at,string,omitempty"`
+}
+
+func main() {
+	var example Example
+	insertedData := Example{Data: "data"}
+
+	db, err := gorm.Open("sqlite3", "examples.db")
+
+	if err != nil {
+		db.AutoMigrate(&example)
+		db.Create(insertedData)
+		fmt.Println("Insert OK!")
+	} else {
+		fmt.Println(err)
+		return
+	}
+	pagination.Paginate(&pagination.Param{
+		DB:      db,
+		Limit:   10,
+		Offset:  1,
+		OrderBy: "id ASC",
+	}, &example)
+
+}
