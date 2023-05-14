a19c1eea9227b185df8ec99622cd44cd828c8715
Md. Masudur Rahman <masudurhimel@gmail.com>
2023-05-14 22:58:52 +0600

    pagination process updated

--- pagination.go
+++ pagination.go
@@ -5,76 +5,51 @@ import (
 	"math"
 )
 
-type Param struct {
-	DB      *gorm.DB
-	Offset  int64
-	Limit   int64
-	OrderBy string
-	Query   interface{}
+type Result[T any] struct {
+	TotalRecord int64 `json:"total_record"`
+	TotalPage   int64 `json:"total_page"`
+	Offset      int64 `json:"offset"`
+	Limit       int64 `json:"limit"`
+	PrevPage    int64 `json:"prev_page"`
+	NextPage    int64 `json:"next_page"`
+	Results     []T   `json:"results"`
 }
 
-type Result struct {
-	TotalRecord int64       `json:"total_record"`
-	TotalPage   int64       `json:"total_page"`
-	Offset      int64       `json:"offset"`
-	Limit       int64       `json:"limit"`
-	PrevPage    int64       `json:"prev_page"`
-	NextPage    int64       `json:"next_page"`
-	Results     interface{} `json:"results"`
-}
+func Paginate[T any](limit, offset int64, db *gorm.DB) (*Result[T], error) {
+	var result Result[T]
+	var data []T
+	var count int64
 
-func Paginate(param *Param, resultData interface{}) *Result {
-	db := param.DB
-
-	var result Result
-	var count, offset int64
-	done := make(chan bool, 1)
-
-	go countResults(db, resultData, done, &count)
-
-	if param.Limit == 0 {
-		param.Limit = 10
+	if limit == 0 {
+		limit = 10
 	}
 
-	if param.Offset == 0 {
-		offset = 0
-	} else {
-		offset = param.Offset
+	err := db.
+		Limit(int(limit)).
+		Offset(int(offset)).
+		Find(&data).Error
+
+	if err != nil {
+		return nil, err
 	}
-
-	if err := param.Query; err == nil {param.Query = "" }
-	
-	db.Offset(int(offset)).
-		Limit(int(param.Limit)).
-		Order(param.OrderBy).
-		Where(param.Query).
-		Find(resultData)
-
-	<-done
 
 	result.TotalRecord = count
-	result.Results = resultData
+	result.Results = data
 
 	result.Offset = offset
-	result.Limit = param.Limit
-	result.TotalPage = int64(math.Ceil(float64(count) / float64(param.Limit)))
+	result.Limit = limit
+	result.TotalPage = int64(math.Ceil(float64(count) / float64(limit)))
 
-	if param.Offset > 1 {
-		result.PrevPage = param.Offset - 1
+	if offset > 1 {
+		result.PrevPage = offset - 1
 	} else {
-		result.PrevPage = param.Offset
+		result.PrevPage = offset
 	}
 
-	if param.Offset == result.TotalPage {
-		result.NextPage = param.Offset
+	if offset == result.TotalPage {
+		result.NextPage = offset
 	} else {
-		result.NextPage = param.Offset + 1
+		result.NextPage = offset + 1
 	}
-	return &result
-}
-
-// count through separate channel
-func countResults(db *gorm.DB, anyType interface{}, done chan bool, count *int64) {
-	db.Model(anyType).Count(count)
-	done <- true
+	return &result, nil
 }
